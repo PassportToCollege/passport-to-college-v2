4219f2cf05f6e3e6bf622dff0eac6356d0ce58f5
Orlando Watson <ogw@orlandogwatson.com>
2018-03-24 14:49:01 -0700

    add create user to reducer

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -2,7 +2,13 @@ import initialState from "./initialState";
 import {
   USERS_GET_INITIATED, 
   USERS_GET_FAILED,
-  USERS_GET_SUCCESS
+  USERS_GET_SUCCESS,
+  USERS_CREATE_USER_INITIATED,
+  USERS_CREATE_USER_FAILED,
+  USERS_NEW_USER_CREATED,
+  USERS_SEND_SIGNUP_EMAIL_INITIATED,
+  USERS_SEND_SIGNUP_EMAIL_FAILED,
+  USERS_SIGNUP_EMAIL_SENT
 } from "../actions/actionTypes";
 
 const users = (state = initialState.users, action) => {
@@ -32,6 +38,50 @@ const users = (state = initialState.users, action) => {
         page: action.page,
         userType: action.userType,
         users: action.users
+      });
+    case USERS_CREATE_USER_INITIATED:
+      return Object.assign({}, state, {
+        isCreating: true,
+        hasCreated: false,
+        hasFailed: false,
+        data: action.data
+      });
+    case USERS_CREATE_USER_FAILED:
+      return Object.assign({}, state, {
+        isCreating: false,
+        hasCreated: false,
+        hasFailed: true,
+        data: action.data,
+        error: action.error
+      });
+    case USERS_NEW_USER_CREATED:
+      return Object.assign({}, state, {
+        isCreating: false,
+        hasCreated: true,
+        hasFailed: false,
+        data: action.data
+      });
+    case USERS_SEND_SIGNUP_EMAIL_INITIATED:
+      return Object.assign({}, state, {
+        isSending: true,
+        hasSent: false,
+        hasFailed: false,
+        email: action.email
+      });
+    case USERS_SEND_SIGNUP_EMAIL_FAILED:
+      return Object.assign({}, state, {
+        isSending: false,
+        hasSent: false,
+        hasFailed: true,
+        email: action.email,
+        error: action.error
+      });
+    case USERS_SIGNUP_EMAIL_SENT:
+      return Object.assign({}, state, {
+        isSending: false,
+        hasSent: true,
+        hasFailed: false,
+        email: action.email
       });
     default:
       return state;
