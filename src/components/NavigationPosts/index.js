6172cd5d61925eeee9a66cacb3e66c8cfd04c448
Orlando Watson <ogw@orlandogwatson.com>
2018-04-02 14:19:07 -0700

    make posts navigation component based

--- src/components/NavigationPosts/index.js
+++ src/components/NavigationPosts/index.js
@@ -1,6 +1,6 @@
 import "./NavigationPosts.css";
 
-import React from "react";
+import React, { Component } from "react";
 import { withRouter, NavLink, Link } from "react-router-dom";
 import propTypes from "prop-types";
 
@@ -9,38 +9,53 @@ import { faPlus, faFilter } from "@fortawesome/fontawesome-free-solid";
 
 import * as routes from "../../constants/routes";
 
-const NavigationPosts = () => {
-  return (
-    <nav className="navigation_posts">
-      <Link className="navigation_posts__add_post" title={routes.DASH_POSTS_NEW.name} 
-        to={routes.DASH_POSTS_NEW.route}>
-        <FontAwesomeIcon icon={faPlus} />
-      </Link>
-      <ul className="navigation_posts__links">
-        {
-          routes.DASH_POSTS_SECTIONS.map(section => {
-            return (
-              <li key={section.route}>
-                <NavLink exact to={section.route}
-                  activeClassName="active">
-                  {section.name.toLowerCase()}
-                </NavLink>
-              </li>
-            )
-          })
-        }
-      </ul>
-      <NavLink className="navigation_posts__categories" title={routes.POST_CATEGORIES.name}
-        to={routes.POST_CATEGORIES.route}
-        activeClassName="active">
-        <FontAwesomeIcon icon={faFilter} />
-      </NavLink>
-    </nav>
-  )
+class NavigationPosts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: props.show
+    }
+  }
+
+  render() {
+    return (
+      <nav className={`navigation_posts ${this.state.show ? "" : "hide"}`}>
+        <Link className="navigation_posts__add_post" title={routes.DASH_POSTS_NEW.name} 
+          to={routes.DASH_POSTS_NEW.route}>
+          <FontAwesomeIcon icon={faPlus} />
+        </Link>
+        <ul className="navigation_posts__links">
+          {
+            routes.DASH_POSTS_SECTIONS.map(section => {
+              return (
+                <li key={section.route}>
+                  <NavLink exact to={section.route}
+                    activeClassName="active">
+                    {section.name.toLowerCase()}
+                  </NavLink>
+                </li>
+              )
+            })
+          }
+        </ul>
+        <NavLink className="navigation_posts__categories" title={routes.POST_CATEGORIES.name}
+          to={routes.POST_CATEGORIES.route}
+          activeClassName="active">
+          <FontAwesomeIcon icon={faFilter} />
+        </NavLink>
+      </nav>
+    )
+  }
+}
+
+NavigationPosts.defaultProps = {
+  show: true
 }
 
 NavigationPosts.propTypes = {
-  addPost: propTypes.func
+  addPost: propTypes.func,
+  show: propTypes.bool
 }
 
 export default withRouter(NavigationPosts);
