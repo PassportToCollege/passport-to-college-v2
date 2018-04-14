107696f620a01ecdb83e4085aa13e21e2e2919ee
Orlando Watson <ogw@orlandogwatson.com>
2018-04-13 18:27:32 -0700

    create link dropdown component

--- src/containers/Stories/index.js
+++ src/containers/Stories/index.js
@@ -8,24 +8,60 @@ import propTypes from "prop-types";
 import * as postCategoryActions from "../../actions/postCategoryActions";
 import * as postsActions from "../../actions/postsActions";
 
+import LinkDropdown from "../../components/LinkDropdown";
+
 class Stories extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      page: 1,
+      posts: props.posts.paginatedPosts,
+      categories: props.postCategories.categories
+    }
   }
 
   componentWillMount() {
     this.props.updateLocation("stories");
     this.props.postsActions.doPostsPaginate(1);
+    this.props.postCategoryActions.doCategoriesGet();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.posts.paginationDone)
+      this.setState({ posts: nextProps.posts.paginatedPosts });
+    
+    if (nextProps.postCategories.gotCategories)
+      this.setState({ categories: nextProps.postCategories.categories });
   }
 
   render() {
     return (
       <main className="stories">
-        
+        <section className="stories__header">
+          {
+            this.props.postCategories.gotCategories && this.state.categories ?
+              <LinkDropdown name="Categories" data={this.createLinkDropdownData()} /> :
+              null
+          }
+        </section>
       </main>
     );
   }
+
+  createLinkDropdownData = () => {
+    let data = [{
+      to: this.props.match.url,
+      label: "All"
+    }];
+    this.state.categories.map(category => {
+      return data.push({
+        to: `/stories/cat/${category.slug}`,
+        label: category.name
+      });
+    });
+
+    return data;
+  };
 }
 
 Stories.propTypes = {
@@ -33,7 +69,8 @@ Stories.propTypes = {
   postCategories: propTypes.object,
   postCategoryActions: propTypes.object,
   posts: propTypes.object,
-  postsActions: propTypes.object
+  postsActions: propTypes.object,
+  match: propTypes.object
 };
 
 const mapStateToProps = state => {
