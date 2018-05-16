6686b582bbd484ac1cac1e1c75c43c4ba427d9c6
Orlando Watson <ogw@orlandogwatson.com>
2018-05-16 16:06:29 -0700

    ssr wroks with facebook share button

--- src/server/index.js
+++ src/server/index.js
@@ -4,19 +4,18 @@ import { Provider } from "react-redux";
 import { StaticRouter } from 'react-router-dom';
 import express from 'express';
 import { renderToString } from 'react-dom/server';
-import Helmet from "react-helmet";
 
 import reducers from "../common/reducers";
 import App from "../common/containers/App";
 
-const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
+import Renderer from "./middleware/renderer";
+
 const server = express();
 
 server
   .disable('x-powered-by')
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/*', (req, res) => {
-    console.log(req.url)
     const context = {};
     const store = createStore(reducers);
 
@@ -31,29 +30,7 @@ server
     if (context.url) {
       res.redirect(context.url);
     } else {
-      const helmet = Helmet.renderStatic();
-      res.status(200).send(
-        `<!doctype html>
-          <html lang="">
-            <head>
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                <meta charset="utf-8" />
-                ${helmet.meta.toString()}
-                ${helmet.title.toString()}
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link href="//fonts.googleapis.com/css?family=Muli:400,600,700,800,900|Nunito:200,400,800,900|Roboto:100,400,400i,500,700,900" rel="stylesheet" />
-                ${assets.client.css
-                  ? `<link rel="stylesheet" href="${assets.client.css}">`
-                  : ''}
-                ${process.env.NODE_ENV === 'production'
-                  ? `<script src="${assets.client.js}" defer></script>`
-                  : `<script src="${assets.client.js}" defer crossorigin></script>`}
-            </head>
-            <body>
-                <div id="root">${markup}</div>
-            </body>
-          </html>`
-      );
+      Renderer(req, res, markup);
     }
   });
 
