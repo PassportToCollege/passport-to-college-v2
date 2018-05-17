2654ca9c9793efea42dbc6542122b672c60e3b2a
Orlando Watson <ogw@orlandogwatson.com>
2018-05-16 18:43:58 -0700

    make drop uploader a class based component

--- src/common/components/DropUploader/index.js
+++ src/common/components/DropUploader/index.js
@@ -1,82 +1,84 @@
 import "./DropUploader.css";
 
-import React from "react";
+import React, { Component } from "react";
 import propTypes from "prop-types";
 
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
 import { faUpload } from "@fortawesome/fontawesome-free-solid";
 
-const DropUploader = props => {
-  const handleDragAreaClick = () => {
+class DropUploader extends Component {  
+  static propTypes = {
+    handleAvatarChange: propTypes.func,
+    disabled: propTypes.bool,
+    uploaderStyles: propTypes.object,
+    dropAreaStyles: propTypes.object,
+    label: propTypes.object,
+    overlay: propTypes.bool,
+    labelStyles: propTypes.object
+  }
+
+  static defaultProps = {
+    overlay: false
+  }
+
+  render() {
+    return (
+      <form className={`form drop__uploader ${this.props.disabled === true ? "disabled" : ""}`} encType="multipart/form-data" style={this.props.uploaderStyles}>
+        {
+          this.props.overlay ?
+            <div className="drop__uploader_overlay"></div> : null
+        }
+        <fieldset disabled={this.props.disabled === true ? "disabled" : null}>
+          <div className="drop__area" 
+            onClick={this.handleDragAreaClick}
+            onDragOver={this.handleDragOver}
+            onDragLeave={this.handleDragLeave}
+            onDrop={this.handleDrop}
+            style={this.props.dropAreaStyles}>
+            <FontAwesomeIcon icon={faUpload} />
+            <input type="file" name="avatar" accept="image/*"
+              ref={input => this.avatartInput = input}
+              onChange={this.handleAvatarChange} />
+            <label style={this.props.labelStyles}>
+              {
+                this.props.label || 
+                <span><b>Choose an image</b> or drag it here</span>
+              }
+            </label>
+          </div>
+        </fieldset>
+      </form>
+    );
+  }
+
+  handleDragAreaClick = () => {
     this.avatartInput.click();
   }
 
-  const handleAvatarChange = () => {
-    if ("function" === typeof props.handleAvatarChange && !props.disabled)
-      props.handleAvatarChange(this.avatartInput);
+  handleAvatarChange = () => {
+    if ("function" === typeof this.props.handleAvatarChange && !this.props.disabled)
+      this.props.handleAvatarChange(this.avatartInput);
   }
 
-  const handleDragOver = e => {
+  handleDragOver = e => {
     e.preventDefault();
 
     e.target.classList.add("item__enter");
   }
 
-  const handleDragLeave = e => e.target.classList.remove("item__enter");
+  handleDragLeave = e => e.target.classList.remove("item__enter");
 
-  const handleDrop = e => {
+  handleDrop = e => {
     e.preventDefault();
     e.stopPropagation();
 
     const files = e.dataTransfer;
 
-    if ("function" === typeof props.handleAvatarChange && !props.disabled)
-      props.handleAvatarChange(files);
+    if ("function" === typeof this.props.handleAvatarChange && !this.props.disabled)
+      this.props.handleAvatarChange(files);
 
-    handleDragLeave(e);
+    this.handleDragLeave(e);
   }
-
-  return (
-    <form className={`form drop__uploader ${props.disabled === true ? "disabled" : ""}`} encType="multipart/form-data" style={props.uploaderStyles}>
-      {
-        props.overlay ?
-          <div className="drop__uploader_overlay"></div> : null
-      }
-      <fieldset disabled={props.disabled === true ? "disabled" : null}>
-        <div className="drop__area" 
-          onClick={handleDragAreaClick}
-          onDragOver={handleDragOver}
-          onDragLeave={handleDragLeave}
-          onDrop={handleDrop}
-          style={props.dropAreaStyles}>
-          <FontAwesomeIcon icon={faUpload} />
-          <input type="file" name="avatar" accept="image/*"
-            ref={input => this.avatartInput = input}
-            onChange={handleAvatarChange} />
-          <label style={props.labelStyles}>
-            {
-              props.label || 
-              <span><b>Choose an image</b> or drag it here</span>
-            }
-          </label>
-        </div>
-      </fieldset>
-    </form>
-  );
 }
-
-DropUploader.defaultProps = {
-  overlay: false
-};
-
-DropUploader.propTypes = {
-  handleAvatarChange: propTypes.func,
-  disabled: propTypes.bool,
-  uploaderStyles: propTypes.object,
-  dropAreaStyles: propTypes.object,
-  label: propTypes.object,
-  overlay: propTypes.bool,
-  labelStyles: propTypes.object
-};
 
 export default DropUploader;
