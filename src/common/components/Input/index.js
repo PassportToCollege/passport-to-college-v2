48ba14100033d10fea57954cbcb3942654a7ce17
Orlando Watson <ogw@orlandogwatson.com>
2018-06-22 21:54:23 -0700

    save input values to state when changed

---
+++ src/common/components/Input/index.js
@@ -0,0 +1,45 @@
+import "./Input.css";
+
+import React, { Component } from "react";
+import propTypes from "prop-types";
+
+class Input extends Component {
+  render() {
+    return (
+      <input className="input" 
+        ref={input => this.input = input}
+        type={this.props.inputType}
+        placeholder={this.props.inputPlaceholder}
+        disabled={this.props.inputDisabled}
+        name={this.props.inputName}
+        onChange={this.onInputChange}
+        onBlur={this.onInputBlur} />
+    )
+  }
+
+  onInputChange = () => {
+    if ("function" === typeof this.props.whenChange)
+      this.props.whenChange(this.input)
+  }
+
+  onInputBlur = () => {
+    if ("function" === typeof this.props.whenBlur)
+      this.props.whenBlur(this.input);
+  }
+}
+
+Input.defaultProps = {
+  inputType: "text",
+  inputDisabled: false
+};
+
+Input.propTypes = {
+  inputType: propTypes.string,
+  inputName: propTypes.string,
+  inputDisabled: propTypes.bool,
+  inputPlaceholder: propTypes.string,
+  whenChange: propTypes.func,
+  whenBlur: propTypes.func
+}
+
+export default Input;
--- src/common/components/SocialShare/index.js
+++ src/common/components/SocialShare/index.js
@@ -4,47 +4,78 @@ import React, { Component } from "react";
 import propTypes from "prop-types";
 
 import IconButton from "../IconButton";
+import Input from "../Input";
+import Button from "../Button";
+import Modal from "../Modal";
 
 class SocialShare extends Component {
   state = {
-    emailing: false
+    emailing: false,
+    name: "",
+    emails: ""
   }
 
   render() {
     return (
-      <div className="social_share">
-        <h4 className="social_share__label">Share:</h4>
+      <React.Fragment>
         {
-          this.props.facebook ?
-            <IconButton icon="facebook" solid
-              buttonTitle="Share on Facebook"
-              doClick={this.shareFacebook}
-              styles={{
-                backgroundColor: "#3B5998",
-                margin: "0.85rem"
-              }} /> : null
+          this.state.emailing ?
+            <Modal doClose={() => this.setState({ emailing: false })}>
+              <h3>Share this story with your friends</h3>
+              <Input inputName="name"
+                inputPlaceholder="Your name" 
+                whenChange={this.handleInputChange} />
+              <p>Add the email addresses below (separated by commas):</p>
+              <Input inputName="emails"
+                inputPlaceholder="eg. johndoe@jd.com, janedoe@jd.com"
+                whenChange={this.handleInputChange} />
+              <Button type="button" solid
+                doClick={() => this.setState({ emailing: false })}
+                text="Cancel"
+                styles={{
+                  backgroundColor: "rgba(51,51,51)",
+                  marginRight: "1em"
+                }} />
+                <Button type="button" solid
+                  doClick={this.shareEmail}
+                  text="Share"
+                  disabled={!this.state.name.length || !this.state.emails.length} />
+            </Modal> : null
         }
-        {
-          this.props.twitter ?
-            <IconButton icon="twitter" solid
-              buttonTitle="Share on Twitter"
-              doClick={this.shareTwitter}
-              styles={{
-                backgroundColor: "#1DA1F2",
-                margin: "0.85rem"
-              }} /> : null
-        }
-        {
-          this.props.mail ?
-            <IconButton icon="email" solid
-              buttonTitle="Email to Friends"
-              doCLick={this.shareEmail}
-              styles={{
-                backgroundColor: "rgb(51,51,51)",
-                margin: "0.85rem"
-              }} /> : null
-        }
-      </div>
+        <div className="social_share">
+          <h4 className="social_share__label">Share:</h4>
+          {
+            this.props.facebook ?
+              <IconButton icon="facebook" solid
+                buttonTitle="Share on Facebook"
+                doClick={this.shareFacebook}
+                styles={{
+                  backgroundColor: "#3B5998",
+                  margin: "0.85rem"
+                }} /> : null
+          }
+          {
+            this.props.twitter ?
+              <IconButton icon="twitter" solid
+                buttonTitle="Share on Twitter"
+                doClick={this.shareTwitter}
+                styles={{
+                  backgroundColor: "#1DA1F2",
+                  margin: "0.85rem"
+                }} /> : null
+          }
+          {
+            this.props.mail ?
+              <IconButton icon="email" solid
+                buttonTitle="Email to Friends"
+                doClick={this.shareEmail}
+                styles={{
+                  backgroundColor: "rgb(51,51,51)",
+                  margin: "0.85rem"
+                }} /> : null
+          }
+        </div>
+      </React.Fragment>
     );
   }
 
@@ -62,8 +93,12 @@ class SocialShare extends Component {
     `);
   }
 
+  handleInputChange = e => {
+    this.setState({ [e.name]: e.value });
+  }
+
   shareEmail = () => {
-    return;
+    this.setState({ emailing: true });
   }
 }
 
