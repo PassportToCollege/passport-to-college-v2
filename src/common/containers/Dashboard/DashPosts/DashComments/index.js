eaf2e265cd10163cbe6352964b827342c4acb724
Orlando Watson <ogw@orlandogwatson.com>
2018-06-24 21:39:06 -0700

    add header to post comments dash

--- src/common/containers/Dashboard/DashPosts/DashComments/index.js
+++ src/common/containers/Dashboard/DashPosts/DashComments/index.js
@@ -6,6 +6,8 @@ import { bindActionCreators } from "redux"
 import propTypes from "prop-types";
 
 import PageMeta from "../../../../components/PageMeta";
+import LoadingText from "../../../../components/LoadingText";
+import Button from "../../../../components/Button";
 
 import * as commentActions from "../../../../actions/commentActions";
 import * as postActions from "../../../../actions/postActions";
@@ -22,6 +24,7 @@ class DashComments extends Component {
   static propTypes = {
     comments: propTypes.object,
     commentActions: propTypes.object,
+    history: propTypes.object,
     match: propTypes.object,
     post: propTypes.object,
     postActions: propTypes.object
@@ -48,8 +51,39 @@ class DashComments extends Component {
               </title>
             } />
         }
+        <header>
+          <h1>Conversations</h1>
+          {
+            this.props.post.hasGotten ?
+              <span className="dash_comments__post_title">{this.props.post.post.title}</span> :
+              <LoadingText options={{
+                class: "block__lines",
+                bg: "transparent",
+                height: "10px",
+                lines: [
+                  { color: "rgba(255,101,97,0.2)", width: "300px" }
+                ]
+              }} />
+          }
+          <Button type="button" solid
+            text="back to post"
+            doClick={this.goBackToPost}
+            styles={{
+              position: "absolute",
+              right: "2em",
+              top: "50%",
+              transform: "translateY(-50%)",
+              backgroundColor: "#FFCB61"
+            }} />
+        </header>
+        <main className="dash_comments">
+        </main>
       </div>
     )
+  }
+
+  goBackToPost = () => {
+    this.props.history.push(`/admin/dashboard/posts/e/${this.state.post}`);
   }
 }
 
