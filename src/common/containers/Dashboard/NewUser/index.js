d613df573b8c233656f6d4d38881fa86b75d5d32
Orlando Watson <ogw@orlandogwatson.com>
2018-07-06 19:45:15 -0700

    save default user data to state

--- src/common/containers/Dashboard/NewUser/index.js
+++ src/common/containers/Dashboard/NewUser/index.js
@@ -16,13 +16,18 @@ import Button from "../../../components/Button";
 import Input from "../../../components/Input";
 import Checkbox from  "../../../components/Checkbox";
 import RadioList from "../../../components/RadioList";
+import WYSIWYGEditor from "../../../components/Editor";
+import Notification from "../../../components/Notification";
 
 class NewUser extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      user: new User().data
+      user: new User().data,
+      notificationClosed: true,
+      hasNotification: false,
+      notification: ""
     }
   }
 
@@ -35,6 +40,15 @@ class NewUser extends Component {
   render() {
     return (
       <ToTopContainer classes="dashboard__container create_user__container">
+        {
+          !this.state.notificationClosed && this.state.hasNotification && this.state.notification.length ?
+            <Notification text={this.state.notification}
+              doClose={() => this.setState({
+                hasNotification: false,
+                notificationClosed: true,
+                notification: ""
+              })} /> : null
+        }
         <PageMeta more={
           <title>New User | Dashboard | Passport to College</title>
         } />
@@ -74,7 +88,7 @@ class NewUser extends Component {
                 { label: "Female", value: "female" },
                 { label: "Male", value: "male" }
               ]}
-              obRadioChange={this.handleGenderSelect} />
+              onRadioChange={this.handleGenderSelect} />
             <FlexContainer>
               <span>
                 <Input inputName="name.first"
@@ -124,6 +138,85 @@ class NewUser extends Component {
               </span>
             </FlexContainer>
           </section>
+          {
+            this.state.user.isStaff ?
+              <section className="create_user__section">
+                <h2>Staff Role</h2>
+                <FlexContainer>
+                  <span>
+                    <Input inputName="role"
+                      inputPlaceholder="Interviewer"
+                      whenBlur={this.handleInputBlur} />
+                    <p className="create_user__input_label">Role</p>
+                  </span>
+                </FlexContainer>
+              </section> : null
+          }
+          {
+            this.state.user.isStudent ?
+              <section className="create_user__section">
+                <h2>Student Information</h2>
+                <FlexContainer>
+                  <span>
+                    <Input inputName="highSchool"
+                      inputPlaceholder="Lorem High School"
+                      whenBlur={this.handleInputBlurStudent} />
+                    <p className="create_user__input_label">High School</p>
+                  </span>
+                  <span>
+                    <Input inputName="university"
+                      inputPlaceholder="Ipsum College"
+                      whenBlur={this.handleInputBlurStudent} />
+                    <p className="create_user__input_label">University</p>
+                  </span>
+                </FlexContainer>
+                <FlexContainer>
+                  <span>
+                    <Input inputName="major"
+                      inputPlaceholder="Biology"
+                      whenBlur={this.handleInputBlurStudent} />
+                    <p className="create_user__input_label">Major</p>
+                  </span>
+                  <span>
+                    <Input inputName="minor"
+                      inputPlaceholder="Chemistry"
+                      whenBlur={this.handleInputBlurStudent} />
+                    <p className="create_user__input_label">Minor (optional)</p>
+                  </span>
+                </FlexContainer>
+                <FlexContainer>
+                  <span>
+                    <Input inputName="enrollment"
+                      inputPlaceholder="2011"
+                      whenBlur={this.handleInputBlurStudent} />
+                    <p className="create_user__input_label">Enrollment Year</p>
+                  </span>
+                  <span>
+                    <Input inputName="graduation"
+                      inputPlaceholder="2015"
+                      whenBlur={this.handleInputBlurStudent} />
+                    <p className="create_user__input_label">Graduation Year</p>
+                  </span>
+                </FlexContainer>
+                <WYSIWYGEditor
+                  captureBlur={this.handleBioBlur} 
+                  editorStyles={{
+                    margin: "0 auto",
+                    maxWidth: "100%",
+                    padding: "2em 1.875em",
+                    backgroundColor: "#FFF",
+                    border: "none"
+                  }} 
+                  controlStyles={{
+                    maxWidth: "100%",
+                    position: "static",
+                    marginBottom: "0"
+                  }} />
+              </section> : null
+          }
+          <Button solid 
+            doClick={this.handleUserSave}
+            text="Save User" />
         </main>
       </ToTopContainer>
     )
@@ -134,6 +227,60 @@ class NewUser extends Component {
         [name]: value
       }) 
     });
+  }
+
+  handleGenderSelect = gender => {
+    this.setState({ 
+      user: Object.assign({}, this.state.user, { gender }) 
+    });
+  }
+
+  handleInputBlur = e => {
+    const { name, value } = e;
+
+    switch (name) {
+      case "name.first":
+        this.setState({ 
+          user: Object.assign({}, this.state.user, {
+            name: Object.assign({}, this.state.user.name, { first: value })
+          }) 
+        });
+        break;
+      case "name.middle":
+        this.setState({ 
+          user: Object.assign({}, this.state.user, {
+            name: Object.assign({}, this.state.user.name, { middle: value })
+          }) 
+        });
+        break;
+      case "name.last":
+        this.setState({ 
+          user: Object.assign({}, this.state.user, {
+            name: Object.assign({}, this.state.user.name, { last: value })
+          }) 
+        });
+        break;
+      case "dob":
+        this.setState({
+          user: Object.assign({}, this.state.user, {
+            dob: new Date(value).getTime()
+          })
+        });
+        break;
+      case "country":
+        this.setState({
+          user: Object.assign({}, this.state.user, {
+            address: { country: value }
+          })
+        });
+        break;
+      default:
+        this.setState({ 
+          user: Object.assign({}, this.state.user, {
+            [name]: value
+          }) 
+        });
+    }
   }
 }
 
