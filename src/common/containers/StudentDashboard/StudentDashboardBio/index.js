08cb1b2b6d380dd8c7f29c8c7c55cefe61cd6151
Orlando Watson <ogw@orlandogwatson.com>
2018-08-16 11:06:34 -0700

    display editor on bio page

--- src/common/containers/StudentDashboard/StudentDashboardBio/index.js
+++ src/common/containers/StudentDashboard/StudentDashboardBio/index.js
@@ -3,6 +3,8 @@ import "./Bio.css";
 import React, { Component } from "react";
 import propTypes from "prop-types";
 
+import WYSIWYGEditor from "../../../components/Editor";
+
 class StudentBio extends Component {
   constructor(props) {
     super(props);
@@ -18,9 +20,40 @@ class StudentBio extends Component {
 
   render() {
     return (
-      <main className="student_dashboard__container student_dashboard__bio">
-
-      </main>
+      <section className="student_dashboard__container student_dashboard__bio">
+        {
+          this.state.student ?
+            <WYSIWYGEditor saveButton
+              content={this.state.student.bio}
+              handleSave={this.handleBioSave}
+              editorStyles={{
+                margin: "0 auto",
+                maxWidth: "100%",
+                padding: "2em 1.875em",
+                backgroundColor: "#FFF",
+                border: "none"
+              }} 
+              controlStyles={{
+                maxWidth: "100%",
+                position: "static",
+                marginBottom: "0"
+              }} /> :
+              <WYSIWYGEditor saveButton
+                handleSave={this.handleBioSave}
+                editorStyles={{
+                  margin: "0 auto",
+                  maxWidth: "100%",
+                  padding: "2em 1.875em",
+                  backgroundColor: "#FFF",
+                  border: "none"
+                }}
+                controlStyles={{
+                  maxWidth: "100%",
+                  position: "static",
+                  marginBottom: "0"
+                }} />
+        }
+      </section>
     )
   }
 }
