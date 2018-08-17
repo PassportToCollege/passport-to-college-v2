80d3a204b82d5444621eb5ef7e1fc55366f96fbb
Orlando Watson <ogw@orlandogwatson.com>
2018-08-17 11:22:01 -0700

    check if user has password account and render based on this

--- src/common/containers/StudentDashboard/StudentDashboardSettings/index.js
+++ src/common/containers/StudentDashboard/StudentDashboardSettings/index.js
@@ -5,11 +5,15 @@ import propTypes from "prop-types";
 import _ from "lodash";
 
 import PageMeta from "../../../components/PageMeta";
+import FlexContainer from "../../../components/FlexContainer";
+import Button from "../../../components/Button";
+
+import { auth } from "../../../utils/firebase";
 
 class StudentSettings extends Component {
   constructor(props) {
     super(props);
-
+    
     this.state = {
       student: props.student.student
     }
@@ -18,6 +22,21 @@ class StudentSettings extends Component {
   static propTypes = {
     student: propTypes.object,
     studentActions: propTypes.object
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        const pwProvider = auth.currentUser.providerData.findIndex(provider => {
+          return provider.providerId === "password";
+        })
+
+        this.setState({ 
+          userProviders: auth.currentUser.providerData,
+          passwordSet: pwProvider > -1
+        });
+      }
+    });
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
@@ -42,7 +61,30 @@ class StudentSettings extends Component {
             <PageMeta route="STUDENT_DASHBOARD" />
         }
         <section className="student_dashboard__container student_dashboard__settings">
-
+          <h4>Account</h4>
+          <p>Make changes to your account settings here</p>
+          <FlexContainer styles={{ justifyContent: "space-between" }}>
+            <span>
+              <p className="type__uppercase type__caption">email</p>
+              {
+                this.state.student ?
+                  <h6>{this.state.student.user.email}</h6> :
+                  null
+              }
+            </span>
+            <Button solid text="change"
+              doClick={this.handleEmailChangeClick} />
+          </FlexContainer>
+          <FlexContainer styles={{ justifyContent: "space-between" }}>
+            <span>
+              <p className="type__uppercase type__caption">password</p>
+              <h6>{ this.state.passwordSet ? "***********" : "not set" }</h6>
+            </span>
+            <Button solid text={ this.state.passwordSet ? "change" : "create" }
+              doClick={this.handlePasswordChangeClick} />
+          </FlexContainer>
+          <h5>Connections</h5>
+          <p>Connect you accounts</p>
         </section>
       </React.Fragment>
     )
