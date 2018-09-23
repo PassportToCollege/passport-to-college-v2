540282beed5a7b1dee74eaa91f7ab26947b02530
Orlando Watson <ogw@orlandogwatson.com>
2018-09-23 15:06:32 -0700

    add topic sectio to about us page

--- src/common/containers/AboutUs/index.js
+++ src/common/containers/AboutUs/index.js
@@ -8,6 +8,8 @@ import propTypes from "prop-types";
 import * as usersActions from "../../actions/usersActions";
 
 import Header from "../../components/Header";
+import TopicSection from "../../components/TopicSection";
+import ToTopContainer from "../../components/ToTopContainer";
 
 import headerImage from "../../assets/images/about_us__header.jpg";
 
@@ -29,14 +31,30 @@ class AboutUs extends Component {
 
   render() {
     return (
-      <React.Fragment>
+      <ToTopContainer>
         <Header 
           scrollEl="about_main"
           background={headerImage} />
-        <main id="about_main">
+        {this.renderMain()}
+      </ToTopContainer>
+    )
+  }
 
-        </main>
-      </React.Fragment>
+  renderMain = () => {
+    return (
+      <main id="about_main">
+        <TopicSection heading="about us"
+        content={
+          <p>
+            We connect with students from all over the world who are beating the odds. Students who, against all types of challenges, have demonstrated tenacity, strong work ethics, will power, integrity and a burning desire to succeed.
+          </p>
+        } 
+        sectionStyles={{
+          width: "1140px",
+          maxWidth: "100%",
+          margin: "0 auto"
+        }} />
+      </main>
     )
   }
 }
