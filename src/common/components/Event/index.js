17455e69467bd7ca954e14c57d5b0dedd986eb4d
Orlando Watson <ogw@orlandogwatson.com>
2018-10-13 10:42:30 -0700

    create getHeader for Event

--- src/common/components/Event/index.js
+++ src/common/components/Event/index.js
@@ -6,6 +6,7 @@ import propTypes from "prop-types";
 import { makeRGB } from "../../utils";
 
 import Button from "../Button";
+import Modal from "../Modal";
 
 class Event extends Component {
   constructor(props) {
@@ -21,25 +22,25 @@ class Event extends Component {
     layout: propTypes.string
   }
 
+  static getDerivedStateFromProps(nextProps, state) {
+    if (nextProps.layout !== state.layout)
+      return { layout: nextProps.layout };
+
+    return null;
+  }
+
   render() {
     return (
-      <div className="event" data-layout={this.state.layout}>
-        <header style={{
-          backgroundColor: "#333",
-          backgroundImage: `url(${this.props.event.hero})`
-        }}>
-          <div style={{
-            backgroundColor: makeRGB(this.props.event.accent, 0.85)
-          }}>
-            <h4>{this.props.event.title}</h4>
-            <h5>@ {this.props.event.where}</h5>
-            <p>{this.props.event.when}</p>
-          </div>
-        </header>
+      <div className="event">
+        {
+          this.state.layout === "expanded" ?
+            this.renderExpanded() : null
+        }
+        {this.getEventHeader()}
         <main>
           <p>{this.props.event.excerpt}</p>
           <Button solid text="more"
-            doClick={this.handleMoreClick}
+            doClick={() => this.setState({ layout: "expanded" })}
             styles={{
               backgroundColor: makeRGB(this.props.event.accent),
               borderColor: makeRGB(this.props.event.accent)
@@ -50,6 +51,36 @@ class Event extends Component {
             }}></span>
         </main>
       </div>
+    )
+  }
+
+  renderExpanded = () => {
+    return (
+      <Modal classes={["event__expanded"]}
+        doClose={() => this.setState({ layout: "card" })}>
+        {this.getEventHeader(false)}
+      </Modal>
+    )
+  }
+
+  getEventHeader = (content = true) => {
+    return (
+      <header style={{
+        backgroundColor: "#333",
+        backgroundImage: `url(${this.props.event.hero})`
+      }}>
+        {
+          content ?
+            <div style={{
+              backgroundColor: makeRGB(this.props.event.accent, 0.85)
+            }}>
+              <h4>{this.props.event.title}</h4>
+              <h5>@ {this.props.event.where}</h5>
+              <p>{this.props.event.when}</p>
+            </div> :
+            null
+        }
+      </header>
     )
   }
 }
