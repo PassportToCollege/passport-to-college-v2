7c89b3d6ccf7b56c65634929c8dec0490d0c2832
Orlando Watson <orwatson@microsoft.com>
2019-03-28 15:47:47 -0700

    create SSID class

---
+++ src/common/imodels/iSSID.ts
@@ -0,0 +1,7 @@
+import iUser from "./iUser";
+import Cookies from "universal-cookie";
+
+export default interface iSSID {
+  User : iUser;
+  cookies : Cookies
+}
---
+++ src/common/models/SSID.ts
@@ -0,0 +1,31 @@
+import Cookies from "universal-cookie";
+
+import iSSID from "../imodels/iSSID";
+import User from "./User";
+
+export default class SSID implements iSSID 
+{
+  User : User;
+  cookies : Cookies;
+
+  constructor(user : User) 
+  {
+    this.User = user;
+    this.cookies = new Cookies();
+  }
+
+  public create() 
+  {
+    this.cookies.set("ssid", this.User.data, {
+      path: "/",
+      maxAge: 60*60*24*3
+    });
+  }
+
+  public destroy() 
+  {
+    this.cookies.remove("ssid", {
+      path: "/"
+    });
+  }
+}
